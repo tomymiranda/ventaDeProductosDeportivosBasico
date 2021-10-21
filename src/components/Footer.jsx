5c1ce79bcc9e216a07844aebacda8093366e0eca
unknown <tomasmiranda98@hotmail.com>
2021-10-21 19:08:29 -0300

    se inicia modificacion del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
+
 const Footer = ({anio}) => {
     //aca se debe tirar javaScript
     //entre {} se ponen constantes q quiero devolver o q figuren
     
     return ( 
-        <h4>El año es: {anio} </h4>
+        <div>
+                
+                <footer  className = "text-white py-4 bg-secondary">
+                    <div className = "container">
+                        <nav className = "row" >
+                            <ul className = "col-12 col-md-3"></ul>
+
+                        </nav>
+                    </div>
+                <h4>El año es: {anio} </h4>
+
+                </footer>
+                
+
+
+        </div>
+        
      );
 }
  
