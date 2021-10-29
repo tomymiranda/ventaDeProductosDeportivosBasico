f148ca83bebbf16e8c557a44192cdb49cbb4ddd2
unknown <tomasmiranda98@hotmail.com>
2021-10-29 16:24:53 -0300

    se puede eliminar de manera correcta 1 solo elemento del mismo producto

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ function App() {
   
   const [productos,guardarProductos] = useState([
 
-    {id:1, articulo:"Botines Nike Mercurial Vapor",precio:15000},
-    {id:2, articulo:"Botines Adidas F50",precio:16000},
-    {id:3, articulo:"Camiseta Seleccion Argentina 2021",precio:8999},
-    {id:4, articulo:"Camiseta Real Madrid 19/20",precio:5000},
-    {id:5, articulo:"Camiseta Manchester United 19/20",precio:5000}
+    
+    {id:1, articulo:"Botines Nike Mercurial Vapor",precio:15000,cantidad:0},
+    {id:2, articulo:"Botines Adidas F50",precio:16000,cantidad:0},
+    {id:3, articulo:"Camiseta Seleccion Argentina 2021",precio:8999,cantidad:0},
+    {id:4, articulo:"Camiseta Real Madrid 19/20",precio:5000,cantidad:0},
+    {id:5, articulo:"Camiseta Manchester United 19/20",precio:5000,cantidad:0}
     ]);
   
   const [changuito,agregarProducto] = useState([])
--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -3,27 +3,42 @@ import { Fragment } from 'react';
 
 const Producto = ({producto,productos,changuito,agregarProducto}) => {
   
-  const {id,articulo,precio} = producto;
+  const {id,articulo,precio,cantidad} = producto;
   //funcion que agrega productos al carrito
-  const seleccionarProducto = (id) =>{
-      const producto = productos.filter(
+  const seleccionarProducto = (id) =>{ 
+    const productoAAgregar = productos.filter(
         producto => producto.id === id
         )[0];
-        agregarProducto([...changuito,producto]); 
-      //console.log(producto);
-        console.log(changuito);
+    const productoEnElChanguito = changuito.filter( producto => producto.id === id)[0];
+   
+    if(productoEnElChanguito != null ){
+      productoEnElChanguito.cantidad = productoEnElChanguito.cantidad + 1; 
+    }
+    else{
+      agregarProducto([...changuito,productoAAgregar]);   
+    }
+    
+     
+    //console.log(producto);
+    console.log(changuito);
     }  
   
   const eliminarProducto = (id) =>{
-    const productos = changuito.filter( producto => producto.id != id);
-    agregarProducto(productos);
-  }  
-  
+    const productoQueQuieroEliminarEnElChanguito = changuito.filter( producto => producto.id === id)[0];
+    if(productoQueQuieroEliminarEnElChanguito.cantidad > 0){
+        productoQueQuieroEliminarEnElChanguito.cantidad = productoQueQuieroEliminarEnElChanguito.cantidad - 1;
+      }
+    else{
+      const productosSinElEliminado = changuito.filter( producto => producto.id != id);
+      agregarProducto(productosSinElEliminado);
+    }
+       
+  } 
 
   return (
         <Fragment>
           <div>
-            <h3>{articulo} - ${precio}</h3>
+            <h3>{articulo} - ${precio} - {cantidad}</h3>
             {
 
               productos
